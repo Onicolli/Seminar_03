10d1b23a4a6debd5fdf57b480152eb3c93cd01cf
onicolli <onicolli>
2023-02-03 00:28:43 +0600

    Do Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+int counter = 1;
+while (counter < Num) {
+    Console.Write(counter * counter + ", ");
+    counter++;
+}
+Console.Write(counter * counter + ". ");
+
