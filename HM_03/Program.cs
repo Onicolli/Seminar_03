d33c60b99095de8f77736cf049b6f5e0a50e02be
onicolli <onicolli>
2023-02-07 19:44:07 +0600

    Do HomeWork_03

---
+++ HM_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное (N): ");
+int N = int.Parse(Console.ReadLine()!);
+int cube = 0;
+int count = 0;
+while (N > count) {
+    count++;
+    cube++;
+    int sum = Convert.ToInt32(Math.Pow(cube, 3));
+    Console.Write($"{sum}, ");
+}
